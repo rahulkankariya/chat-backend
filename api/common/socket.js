7d9073f12b6da5fd38358b8d773ecec2b089757d
rahulkankariya <rahul.kankariya171197@gmail.com>
2025-05-14 11:47:50 +0530

    updated code

--- api/common/socket.js
+++ api/common/socket.js
@@ -2,7 +2,7 @@ const { Server } = require("socket.io");
 const middleware = require("../middleware/middleware");
 const { handleSocketConnection } = require("../socket/socketHandler");
 
-const initializeSocket = (server) => {
+const initializeSocket = (server,redis) => {
     const io = new Server(server, { cors: { origin: "*" }, methods: ["GET", "POST"] });
 
     // Apply authentication middleware
@@ -11,7 +11,10 @@ const initializeSocket = (server) => {
     io.on("connection", (socket) => {
     
         const user = socket.user;
-        
+        socket.on("ping-redis", async () => {
+            const result = await redis.ping();
+            socket.emit("pong-redis", result); // Should be "PONG"
+          });
         if (!user) {
             console.log("Unauthorized socket connection attempt.");
             socket.disconnect(true);
